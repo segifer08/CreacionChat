d8e74315783b6119fde23700c79623f28d1f4783
GPallavicini <gpallavicini@pioix.edu.ar>
2025-08-25 08:44:08 -0300

    Manito
    se agrega media funcion

--- front/src/app/login/page.js
+++ front/src/app/login/page.js
@@ -6,18 +6,33 @@ import { useRouter } from "next/navigation"
 
 
 export default function Login(){
+    var y = 0
+    var x = 0
     const router = useRouter()
 
     function moverse(){
         router.replace("../register")
     }
+
+    function corrobao1(){
+        y+=1
+    }
+
+    function corrobao2(){
+        x+=1
+    }
     
     function loguear(){
-        
+        if (y != 0 && x != 0){
+            fetch("http://localhost:4000/login")
+        }
+        alert
     }
 
     return(<>
     <Form
+        onChange1={corrobao1}
+        onChange2={corrobao2}
         texth1={"Iniciar Sesión"}
         textb={"Inciar Sesión"}
         onClick={loguear}
