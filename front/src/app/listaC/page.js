15872a2e19040627b2382bfbf1a4087635203e91
GPallavicini <gpallavicini@pioix.edu.ar>
2025-08-27 11:44:11 -0300

    Lista de Contactos

--- front/src/app/listaC/page.js
+++ front/src/app/listaC/page.js
@@ -1,5 +1,16 @@
 "use client"
 
+import ContactoR from "@/components/ContactoR"
+
 export default function listaContactos(){
-    
+
+    return(
+        <>
+          { contactos.map(contacto => {
+            <ContactoR mail={contacto.mail} url={contacto.imagen}></ContactoR>
+          })
+
+          }
+        </>
+    )
 }
