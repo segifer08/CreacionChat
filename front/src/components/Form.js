1c7e8f192faef5db3dd3e1d0dad144868cb81718
bolachong <pchong@pioix.edu.ar>
2025-08-27 11:40:26 -0300

    actualizacion css
    css

--- front/src/components/Form.js
+++ front/src/components/Form.js
@@ -8,7 +8,7 @@ export default function Form(props) {
     return(
         <>
             <h1>{props.texth1}</h1>
-            <h4>Mail:</h4>
+            <h4>Mail</h4>
             <Input type={props.type1} onChange={props.onChange1} value={props.value1}></Input>
             <h4>Password</h4>
             <Input type={props.type2} onChange={props.onChange2} value={props.value2}></Input>
