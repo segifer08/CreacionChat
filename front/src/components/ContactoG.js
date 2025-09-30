0aef7430f2b757d50c77307ecb86852bcf5ca3c5
bolachong <pchong@pioix.edu.ar>
2025-09-30 08:25:54 -0300

    pcc
    css

--- front/src/components/ContactoG.js
+++ front/src/components/ContactoG.js
@@ -2,17 +2,20 @@
 
 import Button from "./Button";
 import Input from "./Input";
+import styles from "./ContactoG.module.css"
 
 export default function ContactoG(props) {
     return (
         <>
-            <img src={props.url}></img>
-            <div>
-                <h4>Cambio de Imagen</h4>
-                <Input type={props.type} onChange={props.onChange} value={props.value}></Input>
-                <Button text={props.text} onClick={props.onClick}></Button>
+            <div className={styles.contactog}>
+                <img src={props.url}></img>
+                <div className={styles.contactogtexto}>
+                    <h4>Cambio de Imagen</h4>
+                    <Input type={props.type} onChange={props.onChange} value={props.value}></Input>
+                    <Button text={props.text} onClick={props.onClick}></Button>
+                </div>
+                <h3>{props.mail}</h3>
             </div>
-            <h3>{props.mail}</h3>
         </>
     )
 
